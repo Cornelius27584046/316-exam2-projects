c0b6187854bfd245b3ed20134210e64c55da9c0d
Cornelius27584046 <51083058+Cornelius27584046@users.noreply.github.com>
2019-07-05 13:59:25 +0200

    Add files via upload

---
+++ Exam Proj 2/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace Exam_Proj_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.redOut = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbxWord = new System.Windows.Forms.TextBox();
+            this.btnGo = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // redOut
+            // 
+            this.redOut.Location = new System.Drawing.Point(12, 58);
+            this.redOut.Name = "redOut";
+            this.redOut.Size = new System.Drawing.Size(776, 322);
+            this.redOut.TabIndex = 0;
+            this.redOut.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(104, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Please enter a word:";
+            // 
+            // tbxWord
+            // 
+            this.tbxWord.Location = new System.Drawing.Point(122, 6);
+            this.tbxWord.Name = "tbxWord";
+            this.tbxWord.Size = new System.Drawing.Size(191, 20);
+            this.tbxWord.TabIndex = 2;
+            // 
+            // btnGo
+            // 
+            this.btnGo.Location = new System.Drawing.Point(338, 4);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(127, 48);
+            this.btnGo.TabIndex = 3;
+            this.btnGo.Text = "Go!";
+            this.btnGo.UseVisualStyleBackColor = true;
+            this.btnGo.Click += new System.EventHandler(this.BtnGo_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnGo);
+            this.Controls.Add(this.tbxWord);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.redOut);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox redOut;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbxWord;
+        private System.Windows.Forms.Button btnGo;
+    }
+}
+
---
+++ Exam Proj 2/Form1.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exam_Proj_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //MessageBox.Show(Directory.GetCurrentDirectory().ToString());
+        }
+
+        private void BtnGo_Click(object sender, EventArgs e)
+        {
+            if (System.IO.File.Exists(Directory.GetCurrentDirectory().ToString() + "\\Data1.txt") &&
+                System.IO.File.Exists(Directory.GetCurrentDirectory().ToString() + "\\Data2.txt"))
+            {
+                redOut.AppendText("Data1.txt exist and Data2.txt exist!");
+            }
+            else
+            {
+                if (System.IO.File.Exists(Directory.GetCurrentDirectory().ToString() + "\\Data1.txt"))
+                {
+                    redOut.AppendText("Only Data1.txt exists");
+                    try
+                    {
+                        FileInfo fi = new FileInfo(Directory.GetCurrentDirectory().ToString() + "\\Data2.txt");
+
+                        long fileSize = fi.Length;
+
+                        File.Create(fi.ToString());
+
+                        redOut.AppendText("Data2.txt Created");
+                    }
+                    catch (Exception err)
+                    {
+                        redOut.AppendText(err.ToString());
+                    }
+                }
+                else if (System.IO.File.Exists(Directory.GetCurrentDirectory().ToString() + "\\Data2.txt"))
+                {
+                    redOut.AppendText("Only Data1.txt exists");
+                    try
+                    {
+                        FileInfo fi = new FileInfo(Directory.GetCurrentDirectory().ToString() + "\\Data1.txt");
+
+                        long fileSize = fi.Length;
+
+                        File.Create(fi.ToString());
+
+                        redOut.AppendText("Data1.txt Created");
+                    }
+                    catch (Exception err)
+                    {
+                        redOut.AppendText(err.ToString());
+                    }
+                }
+                else
+                {
+                    redOut.AppendText("Neither Data1.txt nor Data2.txt Exist");
+                    try
+                    {
+                        FileInfo fi = new FileInfo(Directory.GetCurrentDirectory().ToString() + "\\Data1.txt");
+                        FileInfo fi2 = new FileInfo(Directory.GetCurrentDirectory().ToString() + "\\Data2.txt");
+
+                        long fileSize = fi.Length;
+                        long fileSize2 = fi2.Length;
+
+                        File.Create(fi.ToString());
+                        File.Create(fi2.ToString());
+
+                        redOut.AppendText("Data1.txt Created");
+                        redOut.AppendText("Data2.txt Created");
+                    }
+                    catch (Exception err)
+                    {
+                        redOut.AppendText(err.ToString());
+                    }
+                }
+            }
+
+            using (Stream str = new FileStream(Directory.GetCurrentDirectory().ToString() + "\\Data1.txt", FileMode.Create, FileAccess.Write))
+            {
+                using (StreamWriter writer = new StreamWriter(str))
+                {
+                    string words = "nimble yielding far-flung giddy cut eight vulgar degree calculating treatment yell lavish flower fact extra-small rambunctious oranges pleasure yoke judicious disgusting bag number allow dam responsible request toothpaste long lamp tame land beds clean curved great reply type step grubby preach whirl elastic loose moor error macabre ignorant humorous amuse walk meeting dime playground lewd furtive straight bat tail engine forgetful instrument name race kill punch closed discover dear feeling greasy tug shrug cracker spotty drawer hushed develop voiceless iron smell capricious trousers illustrious miniature hall married shock embarrass dress sisters scale diligent nerve spotless slimy clear zesty wine board pat pretend holiday lunchroom zany determined anger secretive achiever table sleepy scattered pen believe smash careless groan travel horn reduce shocking festive toad suppose quicksand grey plain profit foot crazy intelligent questionable slow scribble zephyr hammer rigid false club far start possess story excellent crib periodic correct innocent unique crawl texture expansion room wise nest flat girl clammy record apathetic use sniff point thundering decide terrify education intend carpenter stir bat satisfy certain form group wide-eyed fang untidy legal brake better post grip wide hand suffer color like geese channel sneaky creature muddled smiling wrong jumbled hideous bridge damaged alluring";
+                    writer.WriteLine(words);
+                }
+            }
+
+            using (Stream str = new FileStream(Directory.GetCurrentDirectory().ToString() + "\\Data2.txt", FileMode.Create, FileAccess.Write))
+            {
+                using (StreamWriter writer = new StreamWriter(str))
+                {
+                    string words = "smelly slimy prickly knit third activity attach embarrass dispensable instinctive subtract youthful sweet spray smart quarter shoe mute whistle willing few skirt tearful engine decorous cut undesirable smiling noxious program terrify zany tangible splendid greasy lake mushy clear talented cherry rush tacit remember color cool dog freezing sigh admit burn twig test handy order deranged horses baseball boring mask calculate name battle guarantee pollution homely match attempt protective boat harbor guide ask channel melodic shame squeeze story act cute straw interrupt lazy neck paddle far spicy person reflect complex hate ice trot frightening table listen separate bite-sized gleaming hurt blue";
+                    writer.WriteLine(words);
+                }
+            }
+
+            string word1 = tbxWord.Text;
+
+            using (Stream str = new FileStream(Directory.GetCurrentDirectory().ToString() + "\\Data1.txt", FileMode.Create, FileAccess.ReadWrite))
+            {
+                using (StreamReader reader = new StreamReader(str))
+                {
+                    using (Stream str2 = new FileStream(Directory.GetCurrentDirectory().ToString() + "\\Data2.txt", FileMode.Create, FileAccess.ReadWrite))
+                    {
+                        using (StreamReader reader2 = new StreamReader(str2))
+                        {
+                            try
+                            {
+                                string data1list = reader.ReadLine();
+                                string[] data1 = data1list.Split(' ');
+                                string data2list = reader2.ReadLine();
+                                string[] data2 = data2list.Split(' ');
+
+                                /*FileInfo fi = new FileInfo(Directory.GetCurrentDirectory().ToString() + "\\Data3.txt");
+
+                                long fileSize = fi.Length;
+
+                                File.Create(fi.ToString());*/
+
+                                using (Stream str3 = new FileStream(Directory.GetCurrentDirectory().ToString() + "\\Data3.txt", FileMode.Create, FileAccess.Write))
+                                {
+                                    using (StreamWriter writer2 = new StreamWriter(str3))
+                                    {
+                                        FileSecurity fSecurity = File.GetAccessControl(Directory.GetCurrentDirectory().ToString() + "\\Data3.txt");
+                                        fSecurity.AddAccessRule(new FileSystemAccessRule("everyone", FileSystemRights.ReadAndExecute, AccessControlType.Deny));
+                                        File.SetAccessControl(Directory.GetCurrentDirectory().ToString() + "\\Data3.txt", fSecurity);
+                                        if (data1.Length > data2.Length)
+                                            for (int i = 0; i <= data1.Length; i++)
+                                            {
+                                                if (data2.Length > i)
+                                                {
+                                                    writer2.WriteLine(data1[i] + word1 + data2[i]);
+                                                }
+                                                else
+                                                {
+                                                    writer2.WriteLine(data1[i] + word1);
+                                                }
+                                            }
+                                        else
+                                        {
+                                            for (int i = 0; i <= data2.Length; i++)
+                                            {
+                                                if (data1.Length > i)
+                                                {
+                                                    writer2.WriteLine(data1[i] + word1 + data2[i]);
+                                                }
+                                                else
+                                                {
+                                                    writer2.WriteLine(data2[i] + word1);
+                                                }
+                                            }
+                                        }
+                                        fSecurity = File.GetAccessControl(Directory.GetCurrentDirectory().ToString() + "\\Data3.txt");
+                                        fSecurity.AddAccessRule(new FileSystemAccessRule("everyone", FileSystemRights.FullControl, AccessControlType.Deny));
+                                        File.SetAccessControl(Directory.GetCurrentDirectory().ToString() + "\\Data3.txt", fSecurity);
+                                    }
+                                }
+                            }catch(Exception err)
+                            {
+                                MessageBox.Show(err.ToString());
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
